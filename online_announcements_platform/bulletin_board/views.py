f7af4024164e50d97cbdd57bbf6edd3c164916e2
stinesc <limarevigor@gmail.com>
2019-03-25 19:18:22 +0200

    add routing and logic for actions with favorites announcements

--- online_announcements_platform/bulletin_board/views.py
+++ online_announcements_platform/bulletin_board/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Announcement, Category
 from .serializers import AnnouncementSerializer, CategorySerializer
@@ -13,14 +14,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return obj.author == request.user
 
 
-class ReadOnly(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return False
-
-
 class AnnouncementViewSet(viewsets.ModelViewSet):
     queryset = Announcement.objects.filter(is_hidden=False)
     serializer_class = AnnouncementSerializer
@@ -28,8 +21,28 @@ class AnnouncementViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('category', 'price')
 
+    @action(methods=['post'], detail=True, permission_classes=(permissions.IsAuthenticated,),
+            url_path='add-to-favorites', url_name='add_to_favorites')
+    def add_to_favorites(self, request, pk=None):
+        announcement = self.get_queryset().get(pk=pk)
+        announcement.in_favorites.add(request.user)
+        return Response(status=status.HTTP_200_OK)
 
-class CategoryViewSet(viewsets.ModelViewSet):
+    @action(methods=['delete'], detail=True, permission_classes=(permissions.IsAuthenticated,),
+            url_path='delete-from-favorites', url_name='delete_from_favorites')
+    def delete_from_favorites(self, request, pk=None):
+        announcement = self.get_queryset().get(pk=pk)
+        announcement.in_favorites.remove(request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['get'], detail=False, permission_classes=(permissions.IsAuthenticated,),
+            url_path='favorites', url_name='favorites')
+    def get_favorites_list(self, request):
+        queryset = self.get_queryset().filter(in_favorites=request.user)
+        serializer = AnnouncementSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.exclude(parent=None)
     serializer_class = CategorySerializer
-    permission_classes = (ReadOnly,)
